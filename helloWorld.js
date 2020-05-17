db047bd4cded988f10ad391103cef3667c60659d
alexruma <40611408+alexruma@users.noreply.github.com>
2020-05-17 16:43:01 -0700

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function(req,res){
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, world!');
-}).listen(6993);
+}).listen(6923);
 
 console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
